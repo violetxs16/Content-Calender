1d22edc2e3e7317794b834b494b4141a54fe2ce4
Violeta Solorio <violeta.solorio16@gmail.com>
2024-06-19 20:58:55 -0700

    Starting spring boot content calendar project

---
+++ src/main/java/dev/violet/project1/content_calender/ContentCalenderApplication.java
@@ -0,0 +1,13 @@
+package dev.violet.project1.content_calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCalenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/violet/project1/content_calender/ContentCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.violet.project1.content_calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
